6409eb82571cd5759d458d49bbc2ea0d2df6f3df
wenbinren <5470862@student.swin.edu.au>
2014-11-25 21:28:01 +1100

    fix issue: dashboard/users/index

--- app/views/dashboard/users/index.blade.php
+++ app/views/dashboard/users/index.blade.php
@@ -1,29 +1,48 @@
 @extends('dashboard.master')
 @section('content')
-<div class="col-md-9">
 
-{{ Form::open(array('url' => 'users/' . $userx->id, 'method' =>'put'))}}
+<div class="col-md-9" >
+    <h2>Users-Management</h2>
+    <div>
+    <a href="{{ URL::to('users/create') }}" class="btn btn-success glyphicon glyphicon-plus-sign"> Add</a>
+    </div>
 
- <h1>Edit User {{ $userx->id }}</h1>
-    
-     <div>   
-            {{ Form::label('username', 'Username:') }}
-            {{ Form::text('username', $userx->username) }}
-        <br>
-            {{ Form::label('password', 'Password:') }}
-            {{ Form::text('password', $userx->password) }}
-         <br>
-            {{ Form::label('name', 'Name:') }}
-            {{ Form::text('name', $userx->name) }}
-         <br>
-            {{ Form::label('role_id', 'Role:') }}
-            {{ Form::text('role_id', $userx->role_id) }}
-        <br>
-           
-    </div>        
-            {{ Form::submit('Save!', array('class' => 'btn btn-success')) }}
-    {{ Form::close() }}
+    <div class="table-responsive">
+              <table class="table table-striped table-hover tablesorter table-bordered ">
+                <thead>
+                  <tr>
+                    <th>Username <i class="fa fa"></i></th>
+                    <th>Name <i class="fa fa"></i></th>
+                    <th>Edit <i class="fa fa"></i></th>
+                    <th>Delete <i class="fa fa"></i></th>
+                  </tr>
+                </thead>
+                <tbody>
 
+            @foreach($users as $user)
+            <tr>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->name }}</td>
+
+
+
+                <td><a href="{{ URL::to('users/'. $user->id .'/edit') }}" class="btn btn-primary btn-xs glyphicon glyphicon-pencil">Edit
+
+
+
+                <td>
+
+                    {{ Form::open(array('url' => 'users/' . $user->id)) }}
+
+                    <button class='btn btn-danger btn-xs' type='submit'>
+                    {{ Form::hidden('_method', 'DELETE') }}
+                    <i value="delete" onClick="return confirm('Are you sure?');"><span class="fa fa-times"></span> Delete</button>
+
+                    {{ Form::close() }}
+                </td>
+            @endforeach
+            </tr>
+        </tbody>
 
 </div>
 
